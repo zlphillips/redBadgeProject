a66134aa2b7bee617a7f5b195c3cc9da890a6602
corynnem <corynnem@users.noreply.github.com>
2020-06-19 14:13:01 -0400

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MenuBar from './home/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MenuBar />
     </div>
   );
 }
--- src/home/Navbar.tsx
+++ src/home/Navbar.tsx
@@ -1,1 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Nav, Navbar } from 'react-bootstrap';
+
+const MenuBar = () => {
+
+
+
+    return (
+        <div>
+                <Nav  variant="tabs" defaultActiveKey="/home" >
+                    <Nav.Item>
+                        <Nav.Link href="/home">Active</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Link</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="disabled" >
+                            Disabled
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+        
+        </div>
+    )
+}
+
+export default MenuBar;
