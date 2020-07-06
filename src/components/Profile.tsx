36214d06ffddf98305ec580c2fcbac55734a8d7b
Lia Malone <lia.a.malone@gmail.com>
2020-07-06 19:08:03 -0400

    profile 1.0

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react';
 // import { profile } from 'console';
 import '../css/Profile.css'
 // import { profile } from 'console';
-
+// import {Modal, ModalHeader, ModalBody, Input, Button, ModalFooter} from "reactstrap"
 
 // When the user scrolls the page, execute myFunction
 
@@ -37,18 +37,21 @@ function myFunction() {
 
     
 return (
-    <div>
-          <p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye kitty time jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Groom yourself 4 hours
-</p>
+<div className="mainDiv"
+style={{margin: "auto",border:"1px solid red", padding:"0 5% 0 5%", display:"inline-block"}}>
+    {/* <p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it
+    </p> */}
 <div className="header-cont" id="myHeader" 
-style={{width:"50%", height:"100%", textAlign:"center"}}>
-  <h2>____'s profile</h2>
+style={{width:"100%", height:"10%", textAlign:"center", position:"sticky"}}>
+  <h2>$username's profile</h2>
 </div>
 
-  <p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye kitty time jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Groom yourself 4 hours
+<p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye kitty time jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Groom yourself 4 hours
 </p>
-<p>
-Fooled again thinking the dog likes me chase red laser dot but love blinks and purr purr purr purr yawn, poop on floor and watch human clean up. Chew master's slippers cats making all the muffins so i will ruin the couch with my claws, and catch mouse and gave it as a present snuggles up to shoulders or knees and purrs you to sleep do not try to mix old food with new one to fool me!. Meeeeouw try to jump onto window and fall while scratching at wall for shred all toilet paper and spread around the house. Poop in the plant pot meow or see owner,</p>
+<p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye kitty time jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Groom yourself 4 hours
+</p>
+<p>Gimme attention gimme attention gimme attention gimme attention gimme attention gimme attention just kidding i don't want it anymore meow bye kitty time jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Groom yourself 4 hours
+</p>
 <p>
 Fooled again thinking the dog likes me chase red laser dot but love blinks and purr purr purr purr yawn, poop on floor and watch human clean up. Chew master's slippers cats making all the muffins so i will ruin the couch with my claws, and catch mouse and gave it as a present snuggles up to shoulders or knees and purrs you to sleep do not try to mix old food with new one to fool me!. Meeeeouw try to jump onto window and fall while scratching at wall for shred all toilet paper and spread around the house. Poop in the plant pot meow or see owner,</p>
 <p>
