d88c447e6485eb9c79fab20b11e8cb8cc7890481
corynnem <corynnem@users.noreply.github.com>
2020-07-23 19:20:29 -0400

    multer update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, MouseEvent } from 'react';
 import '../css/Profile.css'
-
+import Peace from '../assets/peace.png'
 
 
 
@@ -26,7 +26,13 @@ function Profile(props: any) {
   }
 
   useEffect(() => fetchProfile(), [])
-
+  const imgStyles = {
+    height: '4vh',
+    color: 'white',
+    fill: 'white',
+    outline: 'none',
+    border: 'none'
+}
   return (
     
     <div className="mainDiv"
@@ -36,6 +42,7 @@ function Profile(props: any) {
          {profile.map((profile: Profile , index: number) => (
             <h1>{profile.bio}</h1>
             ))}
+            <button onClick={props.clearToken}><img src={Peace} style={imgStyles}/></button>
       </div>
     </div>
   )
