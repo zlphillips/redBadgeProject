cd3e0a986e8ccec26993f9e2c0505827a816e51d
zlphillips <60951969+zlphillips@users.noreply.github.com>
2020-07-25 11:15:54 -0400

    Like Button

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -4,14 +4,14 @@ import {Button, Form, FormGroup, Label, Input} from 'reactstrap'
 const Comment = (props: any) => {
     const[description, setDescription] = useState<string>('');
     const[likes, setLikes] = useState<number>();
-    const[owner, setOwner] = useState<number>();
+    const[userId, setUserId] = useState<number>();
     const[postId, setPostId] = useState<number>()
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
         fetch(`http://localhost:3002/redBadge/comment/new-comment`, {
             method: 'POST',
-            body: JSON.stringify({comment: {description: description, likes: likes, owner: owner, postId: postId}}),
+            body: JSON.stringify({comment: {description: description, likes: likes, userId: userId, postId: postId}}),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': props.token
@@ -20,8 +20,8 @@ const Comment = (props: any) => {
         .then((logData) => {
             setDescription('');
             setLikes(undefined);
-            setOwner(props.user);
-            setPostId(props.post)
+            setUserId(props.user.id);
+            setPostId(props.post.id)
             props.fetchAll();
         })
     }
